1640affab1cdb159db02e470dfd3029a5f0a857b
blueberry <vlad.snouk@gmail.com>
2021-11-10 12:09:32 +0200

    init express project

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import app from './Server';
+import logger from './shared/Logger';
+
+// Start the server
+const port = Number(process.env.PORT || 3000);
+app.listen(port, () => {
+  logger.info('Express server started on port: ' + port);
+});
