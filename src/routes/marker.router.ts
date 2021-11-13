a8af25ec1a48ce6e3ab7234e7db6637f4d159d4b
Yevhenii <horugv16@gmail.com>
2021-11-13 17:20:57 +0200

    delete marker

--- src/routes/marker.router.ts
+++ src/routes/marker.router.ts
@@ -4,6 +4,7 @@ import {
   getMarkers,
   addOneMarker,
   getOneMarker,
+  deleteOneMarker,
 } from "../controllers/marker.controler";
 import { asyncMiddleware } from "../middlewares/asyncMiddleware";
 
@@ -13,7 +14,7 @@ markerRouter.get("/get-all-markers", asyncMiddleware(getAllMarkers));
 markerRouter.get("/get-markers", asyncMiddleware(getMarkers));
 markerRouter.get("/get-marker/:id", asyncMiddleware(getOneMarker));
 markerRouter.post("/create-marker", asyncMiddleware(addOneMarker));
-// markerRouter.delete('/delete-marker', asyncMiddleware());
+markerRouter.delete("/delete-marker/:id", asyncMiddleware(deleteOneMarker));
 // markerRouter.put('/edit-marker', asyncMiddleware());
 
 export default markerRouter;
