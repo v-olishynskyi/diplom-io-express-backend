0399cc5101767b174375c1f06edaaa5a65acbbdb
blueberry <vlad.snouk@gmail.com>
2021-11-14 18:55:31 +0200

    feat: added user methods

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,59 +1,127 @@
 import StatusCodes from 'http-status-codes';
 import { Request, Response } from 'express';
 
-import { paramMissingError } from '../shared/constants';
-import { wait } from '../shared/utils';
+import { paramMissingError, ResponseStatus } from '../shared/constants';
 import UserModel from '../models/user/UserModel';
 
-const { BAD_REQUEST, CREATED, OK } = StatusCodes;
+const { BAD_REQUEST, OK } = StatusCodes;
 
-/**
- * Get all users.
- *
- * @param req
- * @param res
- * @returns
- */
-export async function getAllUsers(req: Request, res: Response) {
-  const users = await UserModel.find({});
+export const userControllers = {
+  getAllUsers: async (req: Request, res: Response) => {
+    try {
+      const users = await UserModel.find().populate('markers');
 
-  return res.status(OK).json({ users });
-}
+      return res.status(OK).json({ users });
+    } catch (error) {
+      res.status(500).json({ status: ResponseStatus.FAILED, error });
+    }
+  },
+  getUserById: async (req: Request, res: Response) => {
+    try {
+      const { id } = req.params;
 
-/**
- * Update one user.
- *
- * @param req
- * @param res
- * @returns
- */
-export async function updateOneUser(req: Request, res: Response) {
-  const { user } = req.body;
-  if (!user) {
-    return res.status(BAD_REQUEST).json({
-      error: paramMissingError,
-    });
-  }
-  user.id = Number(user.id);
-  await wait(500);
+      if (!id) {
+        return res.status(BAD_REQUEST).json({
+          error: paramMissingError,
+        });
+      }
 
-  return res.status(OK).end();
-}
+      const user = await UserModel.findById(id).populate('markers');
 
-/**
- * Delete one user.
- *
- * @param req
- * @param res
- * @returns
- */
-export async function deleteOneUser(req: Request, res: Response) {
-  const { id } = req.params;
+      if (!user) {
+        return res.status(400).json({
+          status: ResponseStatus.FAILED,
+          error: {
+            code: 400,
+            message: 'User not found',
+          },
+        });
+      }
 
-  await wait(500);
-  return res.status(OK).end();
-}
+      return res.status(OK).json({ user });
+    } catch (error) {
+      res.status(500).json({ status: ResponseStatus.FAILED, error });
+    }
+  },
+  // getOneUserByFilter: async (req: Request, res: Response) => {
+  //   try {
+  //     const { filter } = req.query;
 
-export async function getOneUser(req: Request, res: Response) {
-  return res.status(400);
-}
+  //     if (!filter) {
+  //       return res.status(BAD_REQUEST).json({
+  //         error: paramMissingError,
+  //       });
+  //     }
+
+  //     const user = await UserModel.find(filter).populate('markers');
+
+  //     if (!user) {
+  //       return res.status(400).json({
+  //         status: ResponseStatus.FAILED,
+  //         error: {
+  //           code: 400,
+  //           message: 'User not found',
+  //         },
+  //       });
+  //     }
+
+  //     return res.status(OK).json({ user });
+  //   } catch (error) {
+  //     res.status(500).json({ status: ResponseStatus.FAILED, error });
+  //   }
+  // },
+  editUser: async (req: Request, res: Response) => {
+    try {
+      const { user } = req.body;
+
+      if (!user) {
+        return res.status(BAD_REQUEST).json({
+          error: paramMissingError,
+        });
+      }
+
+      const newUser = await UserModel.findByIdAndUpdate(
+        user.id,
+        { $set: user },
+        { new: true, populate: 'markers' }
+      );
+
+      return res
+        .status(OK)
+        .json({ status: ResponseStatus.SUCCESS, data: { user: newUser } });
+    } catch (error) {
+      return res.status(500).json({ status: ResponseStatus.FAILED, error });
+    }
+  },
+  deleteUserById: async (req: Request, res: Response) => {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(BAD_REQUEST).json({
+          error: paramMissingError,
+        });
+      }
+      const remove = await UserModel.findByIdAndRemove(id).populate('owner');
+
+      if (!remove) {
+        return res.status(404).json({
+          status: ResponseStatus.FAILED,
+          error: {
+            code: 400,
+            message: 'User not found',
+          },
+        });
+      }
+
+      return res
+        .status(200)
+        .json({ status: ResponseStatus.SUCCESS, data: { remove } });
+    } catch (error) {
+      res.status(500).json({
+        status: ResponseStatus.FAILED,
+        error,
+      });
+    }
+  },
+};
--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,10 +1,18 @@
 import { Router } from 'express';
-import { getAllUsers, getOneUser } from '../controllers/users.controller';
+import { userControllers } from '../controllers/users.controller';
 import { asyncMiddleware } from '../middlewares/asyncMiddleware';
 
 const userRouter: Router = Router();
 
-userRouter.get('/get-all-users', asyncMiddleware(getAllUsers));
-userRouter.get('/get-user', asyncMiddleware(getOneUser));
+userRouter.get('/get-all-users', asyncMiddleware(userControllers.getAllUsers));
+userRouter.get(
+  '/get-user-by-id/:id',
+  asyncMiddleware(userControllers.getUserById)
+);
+userRouter.put('/edit-user', asyncMiddleware(userControllers.editUser));
+userRouter.delete(
+  '/delete-user/:id',
+  asyncMiddleware(userControllers.deleteUserById)
+);
 
 export default userRouter;
