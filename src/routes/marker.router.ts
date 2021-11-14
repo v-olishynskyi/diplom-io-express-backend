afe46e3d22a57b060a4e474b1e1fd2d24dcf7547
Yevhenii <horugv16@gmail.com>
2021-11-14 13:34:41 +0200

    fix + edit marker

--- src/routes/marker.router.ts
+++ src/routes/marker.router.ts
@@ -1,20 +1,14 @@
 import { Router } from "express";
-import {
-  getAllMarkers,
-  getMarkers,
-  addOneMarker,
-  getOneMarker,
-  deleteOneMarker,
-} from "../controllers/marker.controler";
+import { MarkerController } from "../controllers/marker.controler";
 import { asyncMiddleware } from "../middlewares/asyncMiddleware";
 
 const markerRouter = Router();
 
-markerRouter.get("/get-all-markers", asyncMiddleware(getAllMarkers));
-markerRouter.get("/get-markers", asyncMiddleware(getMarkers));
-markerRouter.get("/get-marker/:id", asyncMiddleware(getOneMarker));
-markerRouter.post("/create-marker", asyncMiddleware(addOneMarker));
-markerRouter.delete("/delete-marker/:id", asyncMiddleware(deleteOneMarker));
-// markerRouter.put('/edit-marker', asyncMiddleware());
+markerRouter.get("/get-all-markers", asyncMiddleware(MarkerController.all));
+markerRouter.get("/get-markers", asyncMiddleware(MarkerController.allWithPagination));
+markerRouter.get("/get-marker/:id", asyncMiddleware(MarkerController.find));
+markerRouter.post("/create-marker", asyncMiddleware(MarkerController.create));
+markerRouter.delete("/delete-marker/:id", asyncMiddleware(MarkerController.delete));
+markerRouter.put('/edit-marker/:id', asyncMiddleware(MarkerController.edit));
 
 export default markerRouter;
